b87cb9428e656e62d96f8206d9139dce57738a14
Kurresailakshmareddy <Kurresailakshmareddy@gmail.com>
2023-12-19 18:46:59 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Including packages needed for this application
 const fs = require("fs");
-const inquirer = require("inquirer");
+const inquirer = import ("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -80,7 +80,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log("Creating Professional README.md File...");
-        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+        writeToFile("README.md", generateMarkdown({... responses}));
     });
 }
 init();
